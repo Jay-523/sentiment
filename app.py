81d348c67eb1b60825114d2e51335c499f39efde
Jay-523 <60832779+Jay-523@users.noreply.github.com>
2020-11-06 22:40:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ def sentiment(x):
     return TextBlob(x).sentiment.polarity
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/get')
 def index():
     return render_template('index.html')
 
 
-@app.route('/', methods = ['POST'])
+@app.route('/get', methods = ['POST'])
 def get_val():
     name = request.form['text']
 
     return render_template('pass.html', n = sentiment(name))
 
-app.run(debug = True)
+app.run()
